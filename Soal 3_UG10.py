8e3b36ba4013232f5dd4dc9fb9a2b7b7e5eecd27
gabrielbramm771 <117884203+gabrielbramm771@users.noreply.github.com>
2022-11-24 13:05:28 +0700

    Add files via upload

---
+++ Soal 3_UG10.py
@@ -0,0 +1,13 @@
+
+print("Tentukan Pilihan Anda!!! [1/2]")
+
+
+a =int(input)("Masukan Pilihan yang anda inginkan:,")
+
+b= int(input)("Masukan Bilangan pertama,")
+c= int(input)("Masukan Bilangan kedua,")
+d= int(input)("Masukan Bilangan ketiga,")
+e= int(input)("Masukan Bilangan keempat,")
+
+
+
