970d02b14d683cb929fe919e300410efaa3a8c99
gabrielbramm771 <117884203+gabrielbramm771@users.noreply.github.com>
2022-11-24 13:10:50 +0700

    Add files via upload

---
+++ Soal 2_UG10.py
@@ -0,0 +1,9 @@
+
+angka = int(input("Masukan bilangan pertama:"))
+angka = int(input("Masukan bilangan kedua:"))
+angka = int(input("Masukan bilangan ketiga:"))
+
+if angka == 0:
+    print("Bukan Pytagoras")
+else:
+    print("Pyhtagoras")
